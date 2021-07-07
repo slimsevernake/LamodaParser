903edb42b73af7f1442105a4f2ac66a782d6e5cd
Roman <saeretpk@gmail.com>
2021-07-07 13:26:52 +0300

    Parser finally does something.
    Here code prays Allah for sake of working correct.

--- Modules/Product.py
+++ Modules/Product.py
@@ -2,16 +2,27 @@ import enum
 
 
 class Product:
-    def __init__(self):
-        self.brand = ""
-        self.name = ""
-        self.article = ""
-        self.type = ""
-        self.image = ""
-        self.status = None
-        self.sizes = None
-        self.link = ""
-        self.price = 0
+    def __init__(self, brand="", name="", article="", type="", image_link="", status=None, sizes=None, link="",
+                 price=0):
+        self.brand = brand
+        self.name = name
+        self.article = article
+        self.type = type
+        self.image_link = image_link
+        self.status = ProductStatus.IN_STOCK if status is None else ProductStatus.OUT_OF_STOCK
+        self.sizes = sizes
+        self.link = link
+        self.price = price
+
+    def __str__(self):
+        return f"{self.brand}\n" \
+               f"{self.name} | {self.type}\n" \
+               f"Price: {self.price}\n" \
+               f"Status: {self.status}\n" \
+               f"SKU: {self.article}\n" \
+               f"Link: {self.link}\n" \
+               f"Image: {self.image_link}\n" \
+               f"Sizes: {' '.join([size.get('value', -1) for size in self.sizes])}\n"
 
 
 class ProductStatus(enum.Enum):
