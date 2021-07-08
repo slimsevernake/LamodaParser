7dfe3fd896a3f7413d3e808bf4738dbe04e42d3f
bremka <bonakulik@gmail.com>
2021-07-08 11:50:38 +0300

    Created Event class for event handling

--- Master/Event.py
+++ Master/Event.py
@@ -6,6 +6,28 @@ class EventCode(enum.Enum):
     ERROR = 2
 
 
+class Subscriber:
+    def __call__(self, *args, **kwargs):
+        pass
+
+
+class Event:
+    subscribers: list[Subscriber]
+
+    def __init__(self):
+        self.subscribers = []
+
+    def subscribe(self, sub: Subscriber):
+        self.subscribers.append(sub)
+
+    def unsubscribe(self, sub: Subscriber):
+        self.subscribers.remove(sub)
+
+    def invoke(self, **arguments):
+        for sub in self.subscribers:
+            sub(arguments)
+
+
 class AbstractResponse:
     pass
 
