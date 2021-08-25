805ae9300c349f2a697be998ecbc5198f7dc1214
Roman <saeretpk@gmail.com>
2021-08-25 05:13:05 +0300

    bot_settings setted up to Test server

--- LamodaBot/bot_settings.py
+++ LamodaBot/bot_settings.py
@@ -1,6 +1,6 @@
 bot_settings = {
-    'webhook': "https://discord.com/api/webhooks/869918425592528950/cdHo9UjG5ORvBVJ_bzTDIxiSghk4OO6XEcwB4UkOyUzVsQ0Yf119Ma5SewFbVTRGbIZ2",
-    'token': "ODY5OTQ0OTU1MzYyMjQyNjAw.YQFlYA.sn91kWJmhIpWRmgDA9r5frlow7U",
+    'webhook': "https://discord.com/api/webhooks/879887152622735400/EOM-Ou_kPePmdq11iNUh4mRfW0k6f-wkQubXY3zDC6MaY6gksirSmdzveGZuJ0IUh1PI",
+    'token': "ODc5ODg3NDIyNDQyMzExNzMy.YSWRBQ.-RCaP0MR6775CxmEtFGvjVxME_c",
     'monitor_loop_time': 7,  # seconds
     'update_tags_loop_time': 13,  # seconds
     'bot_prefix': "!"
